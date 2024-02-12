705e6c217240e7c277c42ab97f74f61884498f67
mashuu-redox <mashuu.redox@proton.me>
2024-02-12 06:47:48 -0800

    fixed catppuccin theme for nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,12 +10,16 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
-
 vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+
+require("lazy").setup("plugins")
 
 require("vim-options")
 
-require("lazy").setup("plugins")
+
+
 
 
 
